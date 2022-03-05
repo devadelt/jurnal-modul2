b987bc8b52128739f1f1693a77daac602268a2bb
devadelt <devaraja@student.telkomuniversity.ac.id>
2022-03-05 10:35:12 +0700

    menambahkan layout GUI kalkulator

--- modul2_1302204058/Form1.cs
+++ modul2_1302204058/Form1.cs
@@ -16,5 +16,55 @@ namespace modul2_1302204058
         {
             InitializeComponent();
         }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 1
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 2
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 3
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 4
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 5
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 6
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 7
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 8
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 9
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtText.Text = txtText.Text & 0
+        }
     }
 }
